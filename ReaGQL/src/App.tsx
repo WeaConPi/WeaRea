51e2848b35a32f209bb08c4a549854cb9dd2837e
Vojtěch Baránek <vojtabaranek@gmail.com>
2017-09-11 22:50:33 +0200

    6

--- ReaGQL/src/App.tsx
+++ ReaGQL/src/App.tsx
@@ -12,7 +12,7 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <pre>{JSON.stringify(this.props,null,2)}</pre>
+        <pre>{JSON.stringify(this.props, null, 2)}</pre>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -22,13 +22,13 @@ class App extends React.Component {
 }
 
 export default graphql(gql`
-{
-  days(buildingId:"58ed211899c6b9a3b02e8acd", page:1,pageSize:10){
-  page
-  pageSize
-  content{
-    date
-   }
-  } 
-}
+  {
+    days(buildingId: "58ed211899c6b9a3b02e8acd", page: 1, pageSize: 10) {
+      page
+      pageSize
+      content {
+        date
+      }
+    }
+  }
 `)(App);
