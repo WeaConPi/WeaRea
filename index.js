bb1ee319d5d0b81f112ea840d61c1555225fae51
Vojtěch Baránek <vojtabaranek@gmail.com>
2017-09-13 22:51:02 +0200

    whataboutnow

--- index.js
+++ index.js
@@ -2,43 +2,41 @@
  * Created by Farmas on 01.05.2017.
  */
 
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // Redirect all HTTP traffic to HTTPS
 function ensureSecure(req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    // OK, continue
-    return next();
-  }
-  res.redirect('https://' + req.hostname + req.url);
+    if (req.headers["x-forwarded-proto"] === "https") {
+        // OK, continue
+        return next();
+    }
+    res.redirect("https://" + req.hostname + req.url);
 }
 
 // Handle environments
-if (process.env.NODE_ENV === 'production') {
-  app.all('*', ensureSecure);
+if (process.env.NODE_ENV === "production") {
+    app.all("*", ensureSecure);
 }
 
-app.use('/ReaRest/', express.static(path.join(__dirname, 'ReaRest/build')));
-app.get('/ReaRest/*', function(request, response) {
-  response.sendFile(path.join(__dirname, 'ReaRest/build/index.html'));
+app.use("/ReaRest/", express.static(path.join(__dirname, "ReaRest/build")));
+app.get("/ReaRest/*", function(request, response) {
+    response.sendFile(path.join(__dirname, "ReaRest/build/index.html"));
 });
 
-app.use('/ReaGQL/', express.static(path.join(__dirname, 'ReaGQL/build')));
-app.get('/ReaGQL/*', function(request, response) {
-  response.sendFile(path.join(__dirname, 'ReaGQL/build/index.html'));
+app.use("/ReaGQL/", express.static(path.join(__dirname, "ReaGQL/build")));
+app.get("/ReaGQL/*", function(request, response) {
+    response.sendFile(path.join(__dirname, "ReaGQL/build/index.html"));
 });
 
-app.get('/hello', (request, response) => {
-  response.send('Hello');
+app.get("/hello", (request, response) => {
+    response.send("Hello");
 });
 
 app.listen(PORT, error => {
-  error
-    ? console.error(error)
-    : console.info(
-        `==> 🌎 Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`,
-      );
+    error
+        ? console.error(error)
+        : console.info(" Visit http://localhost:/ " + PORT);
 });
