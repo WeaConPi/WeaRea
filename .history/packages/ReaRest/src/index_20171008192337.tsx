1d028c70da150f4be43c804e9d99b588d0ad8f80
Vojtěch Baránek <vojtabaranek@gmail.com>
2017-10-08 19:58:00 +0200

    Complete rework of setup, should be working
    waiting to https://github.com/rollup/rollup-plugin-commonjs/issues/232

---
+++ .history/packages/ReaRest/src/index_20171008192337.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createBrowserHistory } from 'history';
+import { configureStore } from './store';
+import { App } from "./containers/App/index";
+
+const store = configureStore();
+const history = createBrowserHistory();
+
+ReactDOM.render(
+    <Provider store={store}>
+            <App />
+    </Provider>,
+    document.getElementById('root')
+);
