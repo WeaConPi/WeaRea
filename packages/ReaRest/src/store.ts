e22cae1ea3c9d4622b46dd38221e280c7a536ab2
Vojtěch Baránek <vojtabaranek@gmail.com>
2017-10-09 21:11:22 +0200

    migrate ReaRest to createreactapp scripts

--- packages/ReaRest/src/store.ts
+++ packages/ReaRest/src/store.ts
@@ -1,28 +1,23 @@
 /**
  * Created by Farmas on 26.04.2017.
  */
-import { applyMiddleware, createStore, Store } from "redux";
-import rootReducer, { RootState } from "./reducers";
-import { createEpicMiddleware } from "redux-observable";
-import rootEpics from "./epics";
+import { applyMiddleware, createStore, Store } from 'redux';
+import rootReducer, { RootState } from './reducers';
+import { createEpicMiddleware } from 'redux-observable';
+import rootEpics from './epics';
 
 export function configureStore(initialState?: RootState): Store<RootState> {
   const create = (window as any).devToolsExtension
     ? (window as any).devToolsExtension()(createStore)
     : createStore;
 
-  const epicMiddleware = createEpicMiddleware(rootEpics)
+  const epicMiddleware = createEpicMiddleware(rootEpics);
 
-  const createStoreWithMiddleware = applyMiddleware(epicMiddleware as any)(create);
+  const createStoreWithMiddleware = applyMiddleware(epicMiddleware as any)(
+    create,
+  );
 
-  const store = createStoreWithMiddleware(rootReducer, initialState) as Store<RootState>;
-
-  if (module.hot) {
-    module.hot.accept('./reducers', () => {
-      const nextReducer = require('./reducers');
-      store.replaceReducer(nextReducer);
-    });
-  }
-
-  return store;
+  return createStoreWithMiddleware(rootReducer, initialState) as Store<
+    RootState
+  >;
 }
