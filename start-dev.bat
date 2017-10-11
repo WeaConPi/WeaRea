282e64bc23c18119d476b18572564d5eec9fe2dd
Vojtěch Baránek <vojtabaranek@gmail.com>
2017-10-11 20:57:40 +0200

    bat start

---
+++ start-dev.bat
@@ -0,0 +1,14 @@
+cd packages/ReaRest
+echo Starting REAREST
+start yarn start
+
+cd ..
+cd ReaGQL
+echo Starting REAGQL
+start yarn start
+
+cd ..
+cd ReaRUI
+echo Starting REARUI
+start yarn start
+
